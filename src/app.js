6b4908b4f1a53a406bdc25af10593544af0571ed
NadyLuna <nadyedja@gmail.com>
2020-11-13 09:59:06 -0300

    Resolução de atividades

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
 const express = require("express")
-const bodyParser = require("body-parser")
+//const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 
 const app = express()
+
+mongoose.connect("mongodb+srv://nadyLuna:nlrb1107@cluster0.thgjc.mongodb.net/cursos?retryWrites=true&w=majority", { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+});
+//conexão com o mongo
+let db = mongoose.connection;
+
+// Captura de erro ou sucesso na conexão
+db.on("error", console.log.bind(console, "connection error:"))
+db.once("open", function (){
+  console.log("conexão feita com sucesso.")
+})
 
 //rotas
 const cursos = require("./routes/cursosRoute")
 
 //configurar body parser
-app.use(bodyParser.json());
-// app.use(express.json()); - Podemos usar a propria função de parser de json do express, sem a necessidade de instalar o body parser 
+//app.use(bodyParser.json());
+app.use(express.json()); 
+//Podemos usar a propria função de parser de json do express, sem a necessidade de instalar o body parser 
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
--- src/controllers/cursosController.js
+++ src/controllers/cursosController.js
@@ -1,48 +1,118 @@
-const cursos = require('../models/cursos')
+const cursos = require("../models/cursos");
 
 const getAll = (req, res) => {
-    res.status(200).send({ message: "pegar todos os cursos"})
-}
+    console.log(req.url);
+    cursos.find(function (err, cursos) {
+        if (err)
+            res.status(500).send({
+                message: err.message,
+            });
+
+        res.status(200).send(cursos);
+    });
+};
 
 const getPorTurno = (req, res) => {
-    const parametros = req.query
-    console.log(parametros)
-    res.status(200).send({ message: "pegar todos os cursos por turno"})
-}
+    console.log(req.query)
+    const params = req.query
+
+    console.log(turno);
+    res.status(200).send({
+        message: "pegar todos os cursos por turno",
+    });
+};
 
 const getById = (req, res) => {
-    res.status(200).send({ message: "pegar todos os cursos por id"})
-}
+    const id = req.params.id;
+    cursos.find({id: id,}, (err, curso) => {
+            if (err) {
+                res.status(424).send({
+                    message: err.message,
+                });
+            } else if (curso.length > 0) {
+                res.status(200).send(curso);
+            } else {
+                res.status(404).send({
+                    message: "Curso não encontrado.",
+                });
+            }
+        }
+    );
+};
 
 const getBootcamps = (req, res) => {
-    res.status(200).send({ message: "pegar todos os cursos por que são do tipo bootcamp"})
-}
+    res.status(200).send({
+        message: "pegar todos os cursos por que são do tipo bootcamp",
+    });
+};
 
 const getCursosGratuitos = (req, res) => {
-    res.status(200).send({ message: "pegar todos os cursos que são gratuitos"})
-}
+    res.status(200).send({
+        message: "pegar todos os cursos que são gratuitos",
+    });
+};
 
 const getCursosPagos = (req, res) => {
-    res.status(200).send({ message: "pegar todos os cursos que são pagos"})
-}
+    res.status(200).send({
+        message: "pegar todos os cursos que são pagos",
+    });
+};
 
 const postCurso = (req, res) => {
-    res.status(200).send({ message: "registrar um curso"})
-}
+    cursos.countDocuments((err, count) => {
+        if (err) {
+            res.status(424).send({
+                message: err.message,
+            });
+        } else {
+            let curso = new cursos(req.body);
+            curso.id = count + 1;
+            curso.save((err) => {
+                if (err) {
+                    res.status(424).send({
+                        message: err.message,
+                    });
+                } else {
+                    res.status(200).send({
+                        status: true,
+                        menssagem: "Curso incluído com sucesso!",
+                    });
+                }
+            });
+        }
+    });
+};
 
 const deleteCurso = (req, res) => {
-    res.status(200).send({ message: "remover um curso"})
-}
+    const id = req.params.id;
+    cursos.find({ id }, (err, curso) => {
+        if (curso.length > 0) {
+            cursos.deleteOne({ id }, err => {
+                err ? res.status(424).send({ message: err.message }) : res.status(200).send({
+                    status: true,
+                    mensagem: 'Curso excluído com sucesso'
+                });
+            });
+        } else {
+            res.status(404).send('Curso não encontrado');
+        };
+    });
+};
+
 
 const deleteCursosPorTurno = (req, res) => {
-    const parametros = req.query
-    console.log(parametros)
-    res.status(200).send({ message: "deletar os cursos por turnos"})
-}
+    const parametros = req.query;
+    console.log(parametros);
+    res.status(200).send({
+        message: "deletar os cursos por turnos",
+    });
+};
 
 const putCurso = (req, res) => {
-    res.status(200).send({ message: "atualizar um curso"})
-}
+    res.status(200).send({
+        message: "atualizar um curso",
+    });
+};
 
 module.exports = {
     getAll,
@@ -54,5 +124,5 @@ module.exports = {
     postCurso,
     deleteCurso,
     deleteCursosPorTurno,
-    putCurso
-}
+    putCurso,
+};
